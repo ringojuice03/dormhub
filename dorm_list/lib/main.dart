2818ef689951d8da1f0bbb950694e8bb430ef077
Selwyn <sguiruela@gbox.adnu.edu.ph>
2024-05-09 01:40:23 +0800

    ADJUST: title fontsize

--- dorm_list/lib/main.dart
+++ dorm_list/lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
           ),
           displayMedium: GoogleFonts.inter(
             fontWeight: FontWeight.w900,
-            fontSize: 24.0,
+            fontSize: 18.0,
           ),
           displayLarge: GoogleFonts.inter(
             fontWeight: FontWeight.w900,
