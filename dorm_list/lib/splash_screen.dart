d03878a85d70647b241b7ac5fd2c767979403e15
Selwyn <sguiruela@gbox.adnu.edu.ph>
2024-05-08 23:14:33 +0800

    ADD: splash screen

---
+++ dorm_list/lib/splash_screen.dart
@@ -0,0 +1,69 @@
+import 'package:dorm_list/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+  late final AnimationController animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 2),
+  );
+
+  late final animation = Tween(
+    begin: 0.0,
+    end: 1.0,
+  ).animate(animationController);
+
+  void navigator() async {
+    await Future.delayed(const Duration(seconds: 5), (){});
+
+    if(!mounted) return;
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => const HomePage()), 
+        (route) => false
+    );
+  }
+
+  @override
+  void initState() {
+    animationController.forward();
+    navigator();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FadeTransition(
+          opacity: animation,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'dormhub',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              Text(
+                'Never again to stressful dorm-hunting!',
+                style: Theme.of(context).textTheme.displaySmall,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
